489247b41caf702032c1a5d3761be0c4f5a4e75a
Jan Lehnardt <jan@apache.org>
2015-10-16 19:24:00 +0200

    init

---
+++ src/recover_couchdb.erl
@@ -0,0 +1,18 @@
+-module(recover_couchdb).
+
+%% recover_couchdb: recover_couchdb library's entry point.
+
+-export([main/1]).
+
+
+%% API
+
+main(_Opts) ->
+    ok().
+
+%% Internals
+
+ok() ->
+    io:format("Hello World!~n", []).
+
+%% End of Module.
